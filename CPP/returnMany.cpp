df3cb890f4fc0b9cdf63c8568083ad1950b3b51e
almostdevelopr <minhalam.dev@gmail.com>
2022-04-15 12:12:11 +0530

    sizeof pointers

--- CPP/returnMany.cpp
+++ CPP/returnMany.cpp
@@ -2,16 +2,22 @@
 
 using namespace std;
 
-void getAddMultiply(int a,int b, int *aptr,int *mptr)
+/*void getAddMultiply(int a,int b, int *aptr,int *mptr)
 {
 	*aptr=a+b;
 	*mptr=a*b;
 }
-
+*/
 int main()
 {
-	int x=10,y=20,a,m;
+	/*int x=10,y=20,a,m;
 	getAddMultiply(x,y,&a,&m);
 
-	cout<<"Addition: "<<a<<" "<<"Multiplication: "<<m<<endl;
+	cout<<"Addition: "<<a<<" "<<"Multiplication: "<<m<<endl;*/
+
+	char arr[]={1,2,3};
+	char *ptr=arr;
+
+	cout<<"Size of array: "<<sizeof(arr)<<endl;
+	cout<<"Size of pointer: "<<sizeof(ptr)<<endl;
 }
