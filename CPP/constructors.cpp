ed377dc35352963a43f996dd6ff38b2a8ee13c0e
almostdevelopr <minhalam.dev@gmail.com>
2022-04-16 11:51:58 +0530

    Create constructors.cpp

---
+++ CPP/constructors.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class Point
+{
+private:
+	int x,y;
+public:
+	Point()
+	{
+		x=0;
+		y=0;
+	}
+	Point(int x1,int y1)
+	{
+		x=x1;
+		y=y1;
+	}
+	void print()
+	{
+		cout<<"("<<x<<" "<<y<<endl;
+	}
+}
+
+int main()
+{
+	Point p1,p2(1,3);
+	p1.print();
+	p2.print();
+
+	Point *ptr = new int(1,6);
+	ptr->print();
+
+	return 0;
+}
