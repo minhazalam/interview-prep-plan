17d9b08a43457445ff66f02bee9585a3620c3ccf
almostdevelopr <minhalam.dev@gmail.com>
2022-04-20 14:52:55 +0530

    Update subarrayZeroSum.cpp

--- DataStructures(Basic)/Hashing/subarrayZeroSum.cpp
+++ DataStructures(Basic)/Hashing/subarrayZeroSum.cpp
@@ -1,5 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+bool isSumZero(int arr[],int n)
+{
+	unorder_set<int> s;
+	int pre_sum=0;
+
+	for(int i=0;i<n;i++)
+	{
+		pre_sum+=arr[i];
+		if(s.find(pre_sum)!=s.end())
+			return true;
+		if(pre_sum==0)
+			return true;
+		s.insert(pre_sum);
+	}
+	return false;
+}
+
 int main()
 {
 	return 0;
