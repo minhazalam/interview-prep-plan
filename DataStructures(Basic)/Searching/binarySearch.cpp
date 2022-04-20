e8579a8c1ead14b605e1415c11794d9c4fe408e1
almostdevelopr <minhalam.dev@gmail.com>
2022-04-20 16:21:18 +0530

    Add iterative binary search

--- DataStructures(Basic)/Searching/binarySearch.cpp
+++ DataStructures(Basic)/Searching/binarySearch.cpp
@@ -8,18 +8,31 @@ int recursiveBinarySearch(int arr[],int low,int high,int x)
 	if(arr[mid]==x)
 		return mid;
 	else if(arr[mid]>x)
-		return binarySearch(arr,low,mid-1,x);
+		return recursiveBinarySearch(arr,low,mid-1,x);
 	else
-		return binarySearch(arr,mid+1,high,x);
+		return recursiveBinarySearch(arr,mid+1,high,x);
 }
-
+int iterativeBinarySearch(int arr[],int low,int high,int x)
+{
+	while(low<=high)
+	{
+		int mid=(low+high)/2;
+		if(arr[mid]==x)
+			return mid;
+		else if(arr[mid]>x)
+			high=mid-1;
+		else
+			low=mid+1;
+	}
+	return -1;
+}
 int main()
 {
 	int arr[]={10,20,30,40,50,60};
 	int x=50;
 	int low=0;
 	int high=(sizeof(arr)/sizeof(arr[0]))-1;
-	int res=recursiveBinarySearch(arr,low,high,x);
+	int res=iterativeBinarySearch(arr,low,high,x);
 	cout<<res<<endl;
 	return 0;
 }
