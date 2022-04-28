5e64482b6ec06299a2650307e1a7c366bf343542
almostdevelopr <minhalam.dev@gmail.com>
2022-04-28 21:43:43 +0530

    Add Lomuto partition

--- DataStructures(Basic)/Sorting/bubbleSort.cpp
+++ DataStructures(Basic)/Sorting/bubbleSort.cpp
@@ -112,10 +112,22 @@ int naivePartition(int arr[],int l,int h,int p)
 	int res; //index of pivot element
 }
 
-int lomutoPartition()
+int lomutoPartition(int arr[],int l,int h)
 {
-	
+	int pivot=arr[h];
+	int i=l-1;
+	for(int j=l;j<=h-1;j++)
+	{
+		if(arr[j]<pivot)
+		{
+			i++;
+			swap(arr[i],arr[j]);
+		}
+	}
+	swap(arr[i+1],arr[h]);
+	return (i+1);
 }
+
 void display(int arr[],int n)
 {
 	for(int i=0;i<n;i++)
