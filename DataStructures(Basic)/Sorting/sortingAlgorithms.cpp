e57f9a62eacad05a185b505e7977f4023993937f
almostdevelopr <minhalam.dev@gmail.com>
2022-04-30 15:24:36 +0530

    Add quick sort

--- DataStructures(Basic)/Sorting/sortingAlgorithms.cpp
+++ DataStructures(Basic)/Sorting/sortingAlgorithms.cpp
@@ -128,6 +128,36 @@ int lomutoPartition(int arr[],int l,int h)
 	return (i+1);
 }
 
+int hoaresPartition(int arr[],int l,int h)
+{
+	int pivot=arr[l];
+	int i=l-1,j=h+1;
+	while(true)
+	{
+		do{
+			i++;
+		}while(arr[i]<pivot);
+
+		do{
+			j--;
+		}while(arr[j]>pivot);
+
+		if(i>=j)
+			return j;
+		swap(arr[i],arr[j]);
+	}
+}
+
+void quickSort(int arr[],int l,int h)
+{
+	if(l<h)
+	{
+		int p=hoaresPartition(arr,l,h);
+		quickSort(arr,l,p);
+		quickSort(arr,p+1,h);
+	}
+}
+
 void display(int arr[],int n)
 {
 	for(int i=0;i<n;i++)
