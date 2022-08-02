ffc0a13c01b53b33324dbd816b6e3916cfc62375
almostdevelopr <minhalam.dev@gmail.com>
2022-08-02 16:32:48 +0530

    Traverse singly linked list

--- DataStructures(Basic)/LinkedList/linkedList.cpp
+++ DataStructures(Basic)/LinkedList/linkedList.cpp
@@ -13,11 +13,24 @@ struct Node
 	}
 };
 
+// To traverse the single linked list
+void printList(Node *head)
+{
+	Node *curr=head;
+	while(curr!=NULL)
+	{
+		cout<<curr->data<<" ";
+		curr=curr->next;
+	}
+}
+
 int main()
 {
 	Node *head=new Node(10);
 	head->next=new Node(20);
 	head->next->next=new Node(30);
 	head->next->next->next=new Node(40);
+
+	printList(head);
 	return 0;
 }
