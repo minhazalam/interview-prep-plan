8dade2be9cf9d4eee3b09502cbb01aa9c977267b
almostdevelopr <minhalam.dev@gmail.com>
2022-08-26 15:36:05 +0530

    Delete head in O(n)

---
+++ DataStructures(Basic)/LinkedList/CircularLinkedList/deleteHeadOfCircularLinkedList.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node *next;
+    Node(int x){
+        data=x;
+        next=NULL;
+    }
+};
+
+void printCircularLinkedList(Node *head){
+    if(head==NULL){
+        return;
+    }
+    Node *p=head;
+    do{
+        cout<<p->data<<" ";
+        p=p->next;
+    }while(p!=head);
+}
+
+void printCircularLinkedListDoWhile(Node *head)
+{
+    if(head==NULL)
+    {
+        return;
+    }
+    Node *p=head;
+    do
+    {
+        cout<<p->data<<" ";
+        p=p->next;
+    }while(p!=head);
+}
+
+// Naive approach to delete the head of a circular linked list
+// O(n) -> n: number of nodes in a linked list.
+Node *deleteHeadCircularLinkedList(Node *head){
+    if(head==NULL){
+        return NULL;
+    }
+    if(head->next==head){
+        delete(head);
+        return NULL;
+    }
+    Node *curr=head;
+    while(curr->next!=head){
+        curr=curr->next;
+    }
+    curr->next=head->next;
+    delete(head);
+    return (curr->next);
+}
+
+int main(){
+    Node *head=new Node(10);
+    head->next=new Node(20);
+    head->next->next=new Node(30);
+    head->next->next->next=new Node(40);
+    head->next->next->next->next=new Node(50);
+    head->next->next->next->next->next=head;
+
+    head=deleteHeadCircularLinkedList(head);
+    printCircularLinkedListDoWhile(head);
+
+    cout<<endl;
+    return 0;
+}
