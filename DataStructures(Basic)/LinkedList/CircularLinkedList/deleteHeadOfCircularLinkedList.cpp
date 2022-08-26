b6c81c8cd904857bab8aca798c009ca072b5d459
almostdevelopr <minhalam.dev@gmail.com>
2022-08-26 15:43:54 +0530

    deleteHeadCircularLinkedListEfficient = O(1)

--- DataStructures(Basic)/LinkedList/CircularLinkedList/deleteHeadOfCircularLinkedList.cpp
+++ DataStructures(Basic)/LinkedList/CircularLinkedList/deleteHeadOfCircularLinkedList.cpp
@@ -54,15 +54,30 @@ Node *deleteHeadCircularLinkedList(Node *head){
     return (curr->next);
 }
 
+Node *deleteHeadCircularLinkedListEfficient(Node *head){
+    if(head==NULL){
+        return NULL;
+    }
+    if(head->next==head){
+        delete(head);
+        return NULL;
+    }
+    head->data=head->next->data;
+    Node *temp=head->next;
+    head->next=head->next->next;
+    delete(temp);
+    return head;
+}
+
 int main(){
     Node *head=new Node(10);
     head->next=new Node(20);
     head->next->next=new Node(30);
     head->next->next->next=new Node(40);
-    head->next->next->next->next=new Node(50);
+    head->next->next->next->next=new Node(34);
     head->next->next->next->next->next=head;
 
-    head=deleteHeadCircularLinkedList(head);
+    head=deleteHeadCircularLinkedListEfficient(head);
     printCircularLinkedListDoWhile(head);
 
     cout<<endl;
