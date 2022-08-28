099ce1bf452e549a91cb909afe34dd4fdf12b59a
almostdevelopr <minhalam.dev@gmail.com>
2022-08-28 18:52:46 +0530

    Add stack basic structure

---
+++ DataStructures(Basic)/Stack/stackIntro.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+using namespace std;
+
+struct MyStack
+{
+    int *arr;
+    int cap;
+    int top;
+    MyStack(int cap)
+    {
+        cap = cap;
+        arr = new int[cap];
+        top = -1;
+    }
+
+    void push(int x)
+    {
+        if (top == cap - 1)
+        {
+            cout << "Stack Overflow!\n";
+        }
+        top++;
+        arr[top] = x;
+    }
+
+    int pop()
+    {
+        if(top==-1)
+        {
+            cout<<"Underflow!\n";
+            return -1;
+        }
+        int res=arr[top];
+        top--;
+        return res;
+    }
+
+    int size()
+    {
+        if(top==-1)
+        {
+            cout<<"Underflow!\n";
+            return -1;
+        }
+        return top+1;
+    }
+
+    int peek()
+    {
+        if(top==-1)
+        {
+            cout<<"Underflow!\n";
+            return -1;
+        }
+        return arr[top];
+    }
+
+    bool isEmpty()
+    {
+        return (top==-1);
+    }
+};
+int main()
+{
+    MyStack s(5);
+
+    cout<<s.pop()<<endl;
+    s.push(12);
+    s.push(13);
+    s.push(14);
+    s.push(15);
+
+    cout<<s.peek()<<endl;
+    cout<<s.pop()<<endl;
+    cout<<s.size()<<endl;
+    cout<<s.isEmpty()<<endl;
+
+
+
+    cout << endl;
+    return 0;
+}
