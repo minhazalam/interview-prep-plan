738a2579edcd75226e7ddf5121a4cf0ea09ec992
almostdevelopr <minhalam.learn@gmail.com>
2022-10-13 17:57:00 +0530

    Add linked rep of tree

---
+++ DataStructures(Basic)/Tree/binaryTree.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int key;
+    Node *left;
+    Node *right;
+    Node(int k)
+    {
+        key=k;
+        left=right=NULL;
+    }
+};
+
+int main()
+{
+    Node *root=NULL;
+
+    cout<<endl;
+    return 0;
+}
