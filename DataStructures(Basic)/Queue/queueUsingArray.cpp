4a7994d12a69a9243251c7bd07d5a0df6f9e2f3e
almostdevelopr <minhalam.learn@gmail.com>
2022-10-13 14:29:16 +0530

    Add simple implementation of queue

--- DataStructures(Basic)/Queue/queueUsingArray.cpp
+++ DataStructures(Basic)/Queue/queueUsingArray.cpp
@@ -1,10 +1,75 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// Simple Implementation
+// All are O(1) except for dequeue which is O(n)
+struct Queue
+{
+    int size,cap;
+    int *arr;
+    Queue(int c)
+    {
+        cap=c;
+        arr=new int[cap];
+    }
+
+    bool isFull()
+    {
+        return cap==size;
+    }
+
+    bool isEmpty()
+    {
+        return size==0;
+    }
+
+    void enqueue(int x)
+    {
+        if (isFull())
+        {
+            return;
+        }
+        arr[size]=x;
+        size++;
+    }
+
+    void dequeue()
+    {
+        if (isEmpty())
+        {
+            return;
+        }
+        for (int i=0;i<size-1;i++)
+        {
+            arr[i]=arr[i+1];
+        }
+        size--;
+    }
+
+    int getFront()
+    {
+        if (isEmpty()){
+            return -1;
+        }
+        return 0;
+    }
+
+    int getRear(){
+        if (isEmpty()){
+            return -1;
+        }
+        return size-1;
+    }
+};
+
+
 int main()
 {
-
-
+    Queue q(8);
+    q.enqueue(4);
+    q.enqueue(6);
+    q.enqueue(5);
+    cout<<q.getFront()<<endl;
     cout<<endl;
     return 0;
 }
